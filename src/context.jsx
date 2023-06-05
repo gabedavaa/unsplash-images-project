ac7874fe39c9a1e936a53de6c0282dd8101eb82a
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-06-05 20:08:32 +0400

    adding functionality

--- src/context.jsx
+++ src/context.jsx
@@ -2,19 +2,34 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+
+  const storedDarkMode = localStorage.getItem("darkItem") === "true";
+  return storedDarkMode || prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState("cat");
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
 
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("darkItem", newDarkTheme);
   };
 
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+
   return (
-    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
       {children}
     </AppContext.Provider>
   );
